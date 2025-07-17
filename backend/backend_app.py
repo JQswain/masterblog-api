aa7a16f731f5fb21889364bf0333c45c129216fc
JQSwain <j.q.swain@hotmail.com>
2025-07-17 12:04:09 -0300

    updated backend_app.py to have an add_post route and function, and a delete route and function

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,6 +13,24 @@ POSTS = [
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
+
+@app.route('/api/posts', methods=['POST'])
+def add_post():
+    if not request.json or not 'title' in request.json or not 'content' in request.json:
+        return jsonify({'error': 'Missing required parameter'}), 400
+    request_data = request.get_json()
+    request_data['id'] = (max(post['id'] for post in POSTS) + 1)
+    POSTS.append(request_data)
+    return request_data, 201
+
+
+@app.route('/api/posts/<int:post_id>', methods=['DELETE'])
+def delete_post(post_id):
+    for post in POSTS:
+        if post['id'] == post_id:
+            POSTS.remove(post)
+            return jsonify({'message': f"Post with {post_id} has been deleted successfully."}), 200
+    return jsonify({'message': f"Post with id {post_id} was not found."}), 404
 
 
 if __name__ == '__main__':
